908e0242fc174fd24eca57d0e0614822ad1f1130
Pravesh Niroula <pniroula366@gmail.com>
2023-01-31 12:39:56 +0545

    added about me page

---
+++ src/pages/about/skills.js
@@ -0,0 +1,229 @@
+import {
+    SiJavascript, 
+    SiJava, 
+    SiPython, 
+    SiMongodb, 
+    SiMysql,
+    SiReact,
+    SiNodedotjs,
+    SiExpress,
+    SiHtml5,
+    SiCss3,
+    SiTailwindcss,
+    SiBootstrap,
+    SiMaterialui,
+    SiChakraui,
+    SiGit,
+    SiGithub,
+    SiHeroku,
+    SiNetlify,
+    SiVisualstudiocode,
+    SiPostman,
+    SiFigma,
+
+} from 'react-icons/si';
+
+export const languages =[
+    {
+        id: 1,
+        icon: <SiJavascript
+            // javascript theme color
+            style={{color: '#F7DF1E'}}
+        />,
+        title: 'Javascript',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    },
+    {
+        id: 2,
+        icon: <SiJava 
+            // java theme color
+            style={{color: '#007396'}}
+        />,
+        title: 'Java',
+        link: 'https://www.java.com/en/',
+    },
+    {
+        id: 3,
+        icon: <SiPython 
+            // python theme color
+            style={{color: '#3776AB'}}
+        />,
+        title: 'Python',
+        link: 'https://www.python.org/',
+    },
+];
+
+export const databases =[
+    {
+        id: 1,
+        icon: <SiMongodb
+            // mongodb theme color
+            style={{color: '#47A248'}}
+        />,
+        title: 'MongoDB',
+        link: 'https://www.mongodb.com/',
+    },
+    {
+        id: 2,
+        icon: <SiMysql
+            // mysql theme color
+            style={{color: '#4479A1'}}
+        />,
+        title: 'MySQL',
+        link: 'https://www.mysql.com/',
+    },
+];
+
+export const frameworks =[
+    {
+        id: 1,
+        icon: <SiReact 
+            // react theme color
+            style={{color: '#61DAFB'}}
+        />,
+        title: 'React',
+        link: 'https://reactjs.org/',
+    },
+    {
+        id: 2,
+        icon: <SiNodedotjs 
+            // node theme color
+            style={{color: '#339933'}}
+        />,
+        title: 'Node.js',
+        link: 'https://nodejs.org/en/',
+    },
+    {
+        id: 3,
+        icon: <SiExpress 
+            // express theme color
+            style={{color: '#000000'}}
+        />,
+        title: 'Express',
+        link: 'https://expressjs.com/',
+    },
+];
+
+export const frontEnd =[
+    {
+        id: 1,
+        icon: <SiHtml5 
+            // html theme color
+            style={{color: '#E34F26'}}
+        />,
+        title: 'HTML5',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
+    },
+    {
+        id: 2,
+        icon: <SiCss3 
+            // css theme color
+            style={{color: '#1572B6'}}
+        />,
+        title: 'CSS3',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    },
+    {
+        id: 3,
+        icon: <SiTailwindcss 
+            // tailwind theme color
+            style={{color: '#06B6D4'}}
+        />,
+        title: 'Tailwind CSS',
+        link: 'https://tailwindcss.com/',
+    },
+    {
+        id: 4,
+        icon: <SiBootstrap 
+            // bootstrap theme color
+            style={{color: '#7952B3'}}
+        />,
+        title: 'Bootstrap',
+        link: 'https://getbootstrap.com/',
+    },
+    {
+        id: 5,
+        icon: <SiMaterialui 
+            // material ui theme color
+            style={{color: '#0081CB'}}
+        />,
+        title: 'Material UI',
+        link: 'https://material-ui.com/',
+    },
+    {
+        id: 6,
+        icon: <SiChakraui 
+            // chakra ui theme color
+            style={{color: '#319795'}}
+        />,
+        title: 'Chakra UI',
+        link: 'https://chakra-ui.com/',
+    },
+];
+
+export const others =[
+    {
+        id: 1,
+        icon: <SiGit 
+            // git theme color
+            style={{color: '#F05032'}}
+        />,
+        title: 'Git',
+        link: 'https://git-scm.com/',
+    },
+    {
+        id: 2,
+        icon: <SiGithub 
+            // github theme color
+            style={{color: '#181717'}}
+        />,
+        title: 'Github',
+        link: 'https://github.com',
+    },
+    {
+        id: 3,
+        icon: <SiHeroku 
+            // heroku theme color
+            style={{color: '#430098'}}
+        />,
+        title: 'Heroku',
+        link: 'https://www.heroku.com/',
+    },
+    {
+        id: 4,
+        icon: <SiNetlify 
+            // netlify theme color
+            style={{color: '#00C7B7'}}
+        />,
+        title: 'Netlify',
+        link: 'https://www.netlify.com/',
+    },
+    {
+        id: 5,
+        icon: <SiVisualstudiocode 
+            // vs code theme color
+            style={{color: '#007ACC'}}
+        />,
+        title: 'VS Code',
+        link: 'https://code.visualstudio.com/',
+    },
+    {
+        id: 6,
+        icon: <SiPostman 
+            // postman theme color
+            style={{color: '#FF6C37'}}
+        />,
+        title: 'Postman',
+        link: 'https://www.postman.com/',
+    },
+    {
+        id: 7,
+        icon: <SiFigma 
+            // figma theme color
+            style={{color: '#F24E1E'}}
+        />,
+        title: 'Figma',
+        link: 'https://www.figma.com/',
+    },
+];
+
